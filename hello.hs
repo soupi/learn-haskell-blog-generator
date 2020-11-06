19791d81bb75b2545b8b7f4692349817ae35b4c7
soupi <gilmi@posteo.net>
2020-11-06 16:41:22 +0200

    Adding paragraphs
    We are starting to build a little library for defining html pages.
    
    Our general strategy is to define functions that will take some
    content and convert it into html by wrapping it with the relevant
    tags.
    
    we will also rename `myhtml` to `html_` as a function that creates an
    html document and use `myhtml` as our html page example where we can
    show our progress.
    
    Here I'm ending each "library" function of our html building library
    with `_` as a convention. It's not necessary, but I think it's nice.
    
    Note that since our new paragraph building function `p_` returns a
    string, we can concatenate it with another paragraph.

--- hello.hs
+++ hello.hs
@@ -1,4 +1,10 @@
-main = putStrLn (myhtml "My title" "Hello, function!")
+main = putStrLn myhtml
 
-myhtml title content =
+myhtml =
+  html_ "My title" (p_ "Paragraph #1" <> p_ "Paragraph #2")
+
+html_ title content =
   "<html><head><title>" <> title <> "</title></head><body>" <> content <> "</body></html>"
+
+p_ txt =
+  "<p>" <> txt <> "</p>"
